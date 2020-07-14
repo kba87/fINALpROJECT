85dc6c32698dcc494fc97bafd845948c675cd63b
kba87 <57541889+kba87@users.noreply.github.com>
2020-07-14 22:13:33 +0200

    Update sensor.cpp

--- sensor.cpp
+++ sensor.cpp
@@ -14,87 +14,123 @@ Sensor::Sensor(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Sensor)
 {
-    ui->setupUi(this);
+     ui->setupUi(this);
+     timer = new QTimer(this);
+     arduino = new QSerialPort();
 
-    arduino = new QSerialPort;
-    arduino_is_available = false;
-    arduino_port_name = "";
-    QSerialPort serial;
+     for (auto serialPort: QSerialPortInfo::availablePorts())
+     {
+         ui->portsComboBox-> addItem(serialPort.portName());
+     }
 
-    qDebug() <<"Number of available points" << QSerialPortInfo::availablePorts().length();
-    foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
-        qDebug() <<"Has vendor ID"<<serialPortInfo.hasVendorIdentifier();
-        if (serialPortInfo.hasVendorIdentifier()){
-            qDebug()<<"Vendor ID: "<<serialPortInfo.vendorIdentifier();
+     ui->portsComboBox->model()->sort(0);
+
+     connect(ui->connectionButton, &QPushButton::clicked,this, &Sensor::connectArduino);
+
+//--------------------
+}
+void Sensor::connectArduino()
+{
+
+    if(!arduino->isOpen())
+    {
+        arduino->setPortName(ui->portsComboBox->currentText());
+        if(!arduino->open(QSerialPort::ReadWrite))
+        {
+            QMessageBox::critical(this, "Error", "Could not open serial port!");
+            return;
         }
-        qDebug() <<"Has product ID"<<serialPortInfo.hasProductIdentifier();
-        if (serialPortInfo.hasProductIdentifier()){
-            qDebug()<<"Product ID: "<<serialPortInfo.productIdentifier();
-        }
-    }
 
-    foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
-        if(serialPortInfo.hasVendorIdentifier() && serialPortInfo.hasProductIdentifier()){
-            if(serialPortInfo.vendorIdentifier() == arduino_uno_vendor_id){
-                if(serialPortInfo.productIdentifier() == arduino_uno_product_id){
-                    arduino_port_name = serialPortInfo.portName();
-                    arduino_is_available = true;
-                }}}}
-    if(arduino_is_available){
-        // konfig i otwarcie portu tutaj
-        arduino->setPortName(arduino_port_name);
-        arduino->open(QSerialPort::ReadWrite);
         arduino->setBaudRate(QSerialPort::Baud9600);
         arduino->setDataBits(QSerialPort::Data8);
         arduino->setParity(QSerialPort::NoParity);
         arduino->setStopBits(QSerialPort::OneStop);
-        arduino->setFlowControl(QSerialPort::NoFlowControl);
 
-        if (serial.isOpen() && serial.isWritable())
-              {
-              qDebug() << "Serial is open";
+        ui->connectionButton->setText("Disconnect");
+        ui->connectionStatusLabel->setText("Connected");
 
-              QByteArray output;
-              QByteArray input;
+        ui->dialgroupBox->setEnabled(true);
 
-                while(true)
-                {
-                output = "GET HUM";
-                serial.write(output);
-               serial.flush();
+         connect(timer,SIGNAL(timeout()),this,SLOT(MyFunction()));
+         timer->start(2000);
+         
+      //  QTimer::start()
 
-                serial.waitForBytesWritten(1000);
-                serial.waitForReadyRead(1000);
-
-                input = serial.readAll();
-                qDebug()<<input;
-
-                }
-              }
-         //  return a.exec();
-       }
+        if(!arduino->isWritable())
+        {
+            QMessageBox::critical(this, "Error", "Could not write to serial port!");
+              qDebug()<<"IsWritable";
+            return;
+        }
 
 
-       //----------
 
-
-         //----------
-        //QSerialPort::setPortName("COM3")
-        qDebug()<<"o panie";
-   //-----------
-        qDebug() << QDateTime::currentDateTime().toString("yyyy.MM.dd hh:mm:ss");
-
-/*
-    //----------
-        QObject::connect(arduino, SIGNAL(readyRead()),this,SLOT(readSerial()));
-    }else{
-        QMessageBox::warning(this, "Port error", "Couldn't find the Arduino!");
     }
+    else
+    {
+        arduino->close();
 
-  //  QSerialPort::write()
-  //  QSerialPort::readLine()
-*/
+        ui->connectionButton->setText("Connect");
+        ui->connectionStatusLabel->setText("Disconnected");
+
+        ui->dialgroupBox->setDisabled(true);
+    }
 }
+void Sensor::MyTimer()
+{
+    timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(MySlot()));
+    timer->start(2000);
+    qDebug()<<"2000";
+    connect(this->arduino, SIGNAL(readyRead()), this, SLOT(readFromPort()));
+
+}
+
+void Sensor::MyFunction()
+{
+     qDebug()<<"update";
+     qDebug()<<"GET HUM";
+     arduino->write("GET HUM\n");
+
+     arduino->waitForBytesWritten(2000);
+     arduino->readLine();
+     //-------------
+     QSerialPort serial;
+     if (serial.isOpen() && serial.isWritable())
+     {
+     qDebug() << "Serial is open";
+
+     QByteArray output;
+     QByteArray input;
+
+       while(true)
+       {
+       output = "GET TEMP";
+       serial.write(output);
+      serial.flush();
+
+       serial.waitForBytesWritten(2000);
+       serial.waitForReadyRead(2000);
+
+       input = serial.readAll();
+       qDebug()<<input;
+
+       }
+     }
+//  return a.exec();
+}
+
+
+
+
+void Sensor::MySlot()
+{
+    qDebug()<<"Timer executed";
+    qDebug()<<"GET HUM";
+
+}
+
+
 Sensor::~Sensor()
 {
     if(arduino->isOpen()){
@@ -103,31 +139,6 @@ Sensor::~Sensor()
     delete ui;
 }
 
-
-void Sensor::readSerial()
-{
-    qDebug()<<"Serial port works";
-    QByteArray serialData= arduino->readAll();
-    QString temp = QString::fromStdString(serialData.toStdString());
-    qDebug()<<temp;
-
-}
-
-void Sensor::addToLogs(QString message)
-{
-    QString currentDateTime = QDateTime::currentDateTime().toString("yyyy.MM.dd hh:mm:ss");
-    ui->TextEdit->append(currentDateTime+ "\t" + message);
-}
-
-void Sensor::updateMeasure(QString command)
-{
-    if(arduino->isWritable()){
-        arduino->write(command.toStdString().c_str());
-    }else {
-    qDebug()<<"Couldnt write to serial";
-    }
-
-}
 //serial.write(envoi.toStdString.c_str() );
 //Send defined commands to get current temperature and humidity ("GET TEMP" and "GET HUM") using
 //QSerialPort::write() function and read response using for example QSerialPort::readLine() function
